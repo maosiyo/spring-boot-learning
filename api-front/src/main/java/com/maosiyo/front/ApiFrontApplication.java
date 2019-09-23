65aca4b77c3e5e289f6756b8d0e89ecd8ef40d80
maosiyo <maosiyoo@gmail.com>
2019-09-23 23:45:21 +0800

    [ADD] 模块化开发依赖测试

---
+++ api-front/src/main/java/com/maosiyo/front/ApiFrontApplication.java
@@ -0,0 +1,13 @@
+package com.maosiyo.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFrontApplication.class, args);
+    }
+
+}
